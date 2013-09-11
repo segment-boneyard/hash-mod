b496b6150fd0e627674a21f22b9d06422f9ee486
Ilya Volodarsky <ivolo@mit.edu>
2013-09-10 22:16:57 -0700

    adding tests and changing to hex

--- test/index.js
+++ test/index.js
@@ -3,12 +3,39 @@ var should  = require('should');
 
 describe('hashMod', function () {
 
-  it('#get', function () {
-    var buckets = 100;
-    var mod = hashMod(100, 'md5');
-    var val = mod.get('some string');
-    val.should.be.above(-1);
-    val.should.be.below(buckets);
+  describe('#get', function () {
+    it('should return a number between 0 and 100', function () {
+      var buckets = 100;
+      var mod = hashMod(buckets, 'md5');
+      var trials = 10000;
+      for (var i = 0; i < trials; i += 1) {
+        var val = mod.get('string' + Math.random());
+        val.should.be.above(-1);
+        val.should.be.below(buckets);
+      }
+    });
+
+    it('should be deterministic', function () {
+      var mod = hashMod(100, 'md5');
+      var res = mod.get('string');
+      res.should.equal(64);
+    });
+  });
+
+  describe('#_integerHash', function () {
+    it('should be deterministic', function () {
+      var mod = hashMod(100, 'md5');
+      var res = mod._integerHash('string');
+      res.should.equal(5.505942970225657e+107);
+    });
+  });
+
+  describe('#_hash', function () {
+    it('should be deterministic', function () {
+      var mod = hashMod(100, 'md5');
+      var res = mod._hash('string');
+      res.should.equal('b45cffe084dd3d20d928bee85e7b0f21');
+    });
   });
 
 });
